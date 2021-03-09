2857831349d708a7461ae3f2ee008569959b111c
jsterlingholt <jsterlingholt@me.com>
2021-03-09 18:09:27 +0000

    additional deployment changes

--- pages/url.js
+++ pages/url.js
@@ -1,23 +1,8 @@
 import Link from 'next/link'
 
-{/* Local Imports */}
-import Logic from '../components/logic2'
-
-import Container from 'react-bootstrap/Container';
-
 export default function Test() {
 
   return (
-    <Logic
-      fname= ""
-      lname= ""
-      title= ""
-      company= ""
-      email= ""
-      phone= ""
-      addressStreet=""
-      addressCity=""
-      addressState=""
-      addressZip="" />
+    null
   )
 }
--- pages/vcardtest.js
+++ pages/vcardtest.js
@@ -19,9 +19,6 @@ export default function VCardInput(){
       <QuickResponse.Input.VCard />
 
 
-        <p>poopy mcpooperson</p>
-
-
 
         <Flex
           position="absolute"
